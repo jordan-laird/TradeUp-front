a4ad30cf94f46cb0497f0bac67b95ce2c547929e
jordan-laird <jordanmack.laird@gmail.com>
2018-12-18 15:01:02 -0600

    started front

--- mod4-front/src/App.js
+++ mod4-front/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AlphaVantage } from './config/api_key'
+import { HomePage } from './components/HomePage'
+
+console.log(AlphaVantage)
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -10,6 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+
           </p>
           <a
             className="App-link"
@@ -17,7 +23,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <HomePage />
           </a>
         </header>
       </div>
