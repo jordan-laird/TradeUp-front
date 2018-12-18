863f879bc6e3c6d282fa862c65ff0d28c44ef937
jordan-laird <jordanmack.laird@gmail.com>
2018-12-18 15:49:33 -0600

    Semantic UI added

--- mod4-front/src/components/HomePage.js
+++ mod4-front/src/components/HomePage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export class HomePage extends React.Component{
     render() {
-
+        return(
+            <Button>TEST COMPONENT</Button>
+        )
     }
 }
