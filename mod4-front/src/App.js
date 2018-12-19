540f689a5dd2b04af9a0ed8ece2bb67fdf9c7af8
github username <github email address>
2018-12-19 17:56:52 -0600

    working on routes

--- mod4-front/src/App.js
+++ mod4-front/src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css'
-import { HomePage } from './views/HomePage'
-
+import "./App.css";
+import { HomePage } from "./views/HomePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
-  
   render() {
     return (
-      <div className="App">
-            <HomePage />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <HomePage /> {/*ask about this part */}
+          <Switch>
+            {/* <Route path="/users/:id/edit" component={UserEdit} />
+            <Route path="/users/:id" component={UserDetail} />
+            <Route path="/users" component={Users} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
