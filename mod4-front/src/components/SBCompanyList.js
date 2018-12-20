514a7a4075520801dbe48e137c37321ca2215a25
jordan-laird <jordanmack.laird@gmail.com>
2018-12-20 10:52:45 -0600

    Info Rendering on Selected Company Page

--- mod4-front/src/components/SBCompanyList.js
+++ mod4-front/src/components/SBCompanyList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SBCurrentPrices } from './SBCurrentPrices'
-import { Container, Divider } from 'semantic-ui-react'
+import { Container, Divider } from 
 
 export class SBCompanyList extends React.Component{
     render(){
