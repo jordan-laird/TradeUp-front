82bac2828a24a470c8f011781db7e61e263ed521
jordan-laird <jordanmack.laird@gmail.com>
2019-01-03 16:14:24 -0600

    shit is working...finally

--- mod4-front/src/App.js
+++ mod4-front/src/App.js
@@ -13,25 +13,39 @@ import { PrivateRoute } from "./components/PrivateRoute";
 // import { Logout } from "./components/Logout";
 
 class App extends Component {
+  state = {
+    currentPrices: []
+  }
   fetchCurrentPrice = (companySymbol) => {
-    fetch(`https://api.iextrading.com/1.0/stock/${companySymbol}/chart/1d`)
+    fetch('https://api.iextrading.com/1.0//stock/market/batch?symbols=aapl,fb,tsla,ba,brk.b,dis,ge,hd,nke,sbux,dji,amzn,baba,goog,nflx,adbe,ftnt,grub,irbt,mcd&types=chart&range=1d')
       .then(res => res.json())
-      .then(prices => prices.length ?
-        this.setState({ currentPrice: prices[prices.length - 1] }) : null)
+      .then(prices => {
+        for( let symbol in prices){
+          let chart = prices[symbol].chart
+          prices[symbol] = chart[chart.length-1]
+        }
+        console.log(prices)
+        this.setState({currentPrices: prices})
+      })
+      // .then(prices => prices.length ?
+      //   this.setState({ currentPrice: prices[prices.length - 1] }) : null)
   }
 
+  componentDidMount(){
+    this.fetchCurrentPrice()
+    setInterval(this.fetchCurrentPrice, 30000)
+  }
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <NavBar />
           <Switch>
-            <PrivateRoute path="/companies" component={HomePage} />
+          <PrivateRoute path="/companies" render={(props) => <HomePage {...props} currentPrices={this.state.currentPrices} />} />
             <Route path="/login" component={Login} />
-            {/* <Route path="/logout" component={Logout} /> */}
             <PrivateRoute path="/users/:id/edit" component={UserEdit} />
             <PrivateRoute path="/users" component={User} />
-            <PrivateRoute path="/Portfolio" component={Portfolio} />
+            <PrivateRoute path="/Portfolio" render={(props) => <Portfolio {...props} currentPrices={this.state.currentPrices} /> } />
             <Route path="/SignUp" component={SignUp} />
           </Switch>
         </div>
--- mod4-front/src/components/Portfolio.js
+++ mod4-front/src/components/Portfolio.js
@@ -4,6 +4,7 @@ import { ActivePortfolioTable } from './ActivePortfolioTable'
 import { InactivePortfolioTable } from './InactivePortfolioTable'
 
 const url = 'https://api.iextrading.com/1.0//stock/market/batch?symbols=aapl,fb,tsla,ba,brk.b,dis,ge,hd,nke,sbux,dji,amzn,baba,goog,nflx,adbe,ftnt,grub,irbt,mcd&types=chart&range=1d'
+const userID = localStorage.getItem("userID")
 
 export class Portfolio extends Component {
   constructor(){
@@ -18,7 +19,7 @@ export class Portfolio extends Component {
     fetch('http://localhost:3001/api/v1/transactions')
     .then(res => res.json())
     .then((transactions) => {
-      myTransactions = transactions.filter(transaction => transaction.user_id === 1)
+      myTransactions = transactions.filter(transaction => transaction.user_id = userID)
       this.setState({ myTransactionList: myTransactions })
      
       //TODO SWITCH TO CURRENT USER
@@ -37,9 +38,10 @@ export class Portfolio extends Component {
       },
       body: JSON.stringify({
         status: false,
+        sold_price: this.props.currentPrices[transactionInfo.stock_symbol] ? this.props.currentPrices[transactionInfo.stock_symbol].close : 0
         // TODO SOLD PRICE
 
-        // TODO NEED TO CHANGE TO LOGGED IN USER
+        
       })
 
     })
@@ -49,6 +51,7 @@ export class Portfolio extends Component {
   
 
   componentDidMount(){
+    console.log(userID)
     this.fetchTransactions()
   }
   render() {
@@ -57,7 +60,7 @@ export class Portfolio extends Component {
         <br />
         <br />
         <h1>Current Portfolio</h1>
-        <ActivePortfolioTable myTransactionList={this.state.myTransactionList} sellShare={this.sellShare}/>
+        <ActivePortfolioTable myTransactionList={this.state.myTransactionList} sellShare={this.sellShare} currentPrices={this.props.currentPrices}/>
         <h1>Previously Owned</h1>
         <InactivePortfolioTable myTransactionList={this.state.myTransactionList}/>
       </div>
